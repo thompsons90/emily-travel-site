b6fc6d0fe547c3300fc75eef27eafb5a42e44425
Scott E Thompson <sthompson082290@gmail.com>
2025-03-12 17:08:15 -0500

    Initial commit with typscript built using vite including jest and playwright testing

---
+++ tests/api-tests/gmail.service.spec.ts
@@ -0,0 +1,8 @@
+// import {test, expect} from '@playwright/test';
+// import { google } from 'googleapis';
+// import { OAuth2Client } from 'google-auth-library';
+// import * as fs from 'fs';
+
+// test('API call returns successful status', async ({ request }) => {
+//     const response = await request.get('')
+// })
